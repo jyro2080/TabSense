5c521927bf9eaec58c3d5eddf9939dbe98587266
Jayesh Salvi <jayeshsalvi@gmail.com>
2010-07-30 16:30:14 +0530

    show only tab titles, instead of preview screenshots
    preview screen shots don't work that well and of less use.
    also reorg the code into separate script.

---
+++ scripts/tabsense.js
@@ -0,0 +1,48 @@
+
+function processTabs(tabs) {
+    var tl = tabs.length;
+    for(var j=0; j < tl; j++) {
+        var tab = tabs[j];
+        var wid = tab.windowId
+        var tid = tab.id;
+
+        var mtab = $('<div></div>').attr('class','mtab');
+
+        if(tab.favIconUrl) {
+            var favicon = $('<img/>');
+            favicon.attr('src', tab.favIconUrl)
+                        .attr('class','favicon')
+                        .width('24px')
+                        .height('24px');
+            mtab.append(favicon);
+        }
+
+        tabtitle = $('<div></div>')
+                .attr('class','title').text(tab.title);
+        mtab.append(tabtitle);
+
+        windowMap[wid].append(mtab);
+    }
+}
+
+var windowMap = [];
+
+$(document).ready(function(){
+
+    chrome.windows.getAll(null, 
+        function(windows) {
+
+            var wl = windows.length;
+            for(var i=0; i < wl; i++) {
+
+                var mwin = $('<div></div>').attr('class','mwin')
+                            .text(''+windows[i].id);
+                windowMap[windows[i].id] = mwin; 
+                $('body').append(mwin);
+
+                chrome.tabs.getAllInWindow(windows[i].id, processTabs);
+            }
+        }
+    );
+    
+});
