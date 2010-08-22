d03f10f8169eae188161369f71bb6191ae3ab50b
Jayesh Salvi <jayesh@tuxmac.(none)>
2010-08-22 16:02:27 +0530

    make heights of element dynamic for popup too

--- src/scripts/ui/popup.js
+++ src/scripts/ui/popup.js
@@ -1,9 +1,9 @@
 var $c = console;
 
 var NUMCOL = 1;
-var wframe;
 var bgport;
 var inPopup = true;
+var windb = null;
 
 function open_channel() {
   bgport = chrome.extension.connect({ name:'ui2bg' });
@@ -11,18 +11,28 @@ function open_channel() {
 
   bgport.onMessage.addListener(function(reply) {
     if(reply.name == 'getcurwindow') {
-      wframe = new WinFrame({ wid:reply.wid, title:reply.title });
+      windb = { wid : reply.wid, title : reply.title };
       bgport.postMessage({ name:'listtabs', wid:reply.wid });
     } else if(reply.name == 'listtabs') {
-      $c.assert(wframe);
-      wframe.addTabs(reply.tabs);
+      $c.assert(windb);
       set_document_height(reply.tabs.length);
+
+      wframe = new WinFrame({ wid:windb.wid, title:windb.title });
+      wframe.addTabs(reply.tabs);
     }
   });
 }
 
 function set_document_height(numtabs) {
-  $('body').css('minHeight', (50+numtabs*45)+'px');
+  var maxH = screen.height - 100;
+  var h = 60 + numtabs*(UI.TAB_HEIGHT+2*UI.TAB_PADDING);
+  while(h > maxH) {
+    UI.scale--;
+    UI.setSize();
+    h = 60 + numtabs*(UI.TAB_HEIGHT+2*UI.TAB_PADDING);
+  }
+  $('body').css('font-size', UI.FONT_SIZE+'px');
+  $('body').css('minHeight', h+'px');
 }
 
 $(document).ready(function(){
